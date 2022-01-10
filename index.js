1de6e3390e85e0defc7059720f07ace13013637e
almar <a.martin@clysema.com>
2022-01-10 12:37:24 +0100

    testeado, envia temperaturas

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ require('dotenv').config();
 const mqtt = require('mqtt');
 let interval;
 
+if (!process.env.THINGSBOARD_HOST) {
+    console.log("ERROR: THINGSBOARD_HOST no está configurado en .env");
+    process.exit(1)
+}
+if (!process.env.ACCESS_TOKEN) {
+    console.log("ERROR: ACCESS_TOKEN no está configurado en .env");
+    process.exit(1)
+}
+
 console.log('Connecting to: %s using access token: %s', process.env.THINGSBOARD_HOST, process.env.ACCESS_TOKEN);
 
 var client  = mqtt.connect('mqtt://'+ process.env.THINGSBOARD_HOST,{
@@ -14,12 +23,12 @@ client.on('connect', () => {
     console.log('Client connected!');
 
     // publicar atributos
-    const attr = { atributo1: "Atributo 1" }
+    const attr = { periodo: 5 }
     client.publish('v1/devices/me/attributes', JSON.stringify(attr));
     console.log('Attributes published!');
 
     if (interval === undefined) {
-        insterval = setInterval(() => {
+        interval = setInterval(() => {
             // publicar telemetría
             const telemetry = {
                 temperatura: Math.floor(Math.random() * 30) + 20
@@ -30,28 +39,10 @@ client.on('connect', () => {
     }
 });
 
+client.on('message', (topic, message) => {
+    console.log('mensaje recibid!', topic,message);
+});
+
 client.on('end', () => {
     console.log('Client end!');
 });
-
-
-
-
-// https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
-//do something when app is closing
-const cleanExit = () => {
-    clearInterval(interval);
-    client.end();
-}
-
-const exitHandler = (options, exitCode) => {
-    if (options.cleanup) console.log('clean');
-    if (exitCode || exitCode === 0) console.log(exitCode);
-    if (options.exit) process.exit();
-}
-
-// capturamos ctrl-C
-process.on('SIGINT', exitHandler.bind(() => cleanExit(), { cleanup: true } ));
-
-//catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(() => cleanExit(), {exit:true}));
